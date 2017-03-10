240ccceecab156f656117a042cc667a6c143b838
anhvurz90 <anhvurz90@gmail.com>
2017-03-10 14:07:23 +0700

    Add tutorial#1: Introduction

---
+++ tutorial/tutorial.java
@@ -0,0 +1,38 @@
+1.Introduction: {
+	1.1.What is Angular2: {
+		- JavaScript framework for creating dynamic web apps
+		- Component based (not typical MVC)
+		- Written with TypeScript
+		- Quite different from Angular1
+	}
+	1.2.Before you start: {
+		- Node package manager
+		- JavaScript / TypeScript
+		- HTML / CSS
+	}
+	1.3.In This Playlist: {
+		- Core features of Angular2 and how it works: {
+			+ Components
+			+ Directives
+			+ Templates
+		}
+		- Create a simple directory app: {
+			+ Routing
+			+ Services
+			+ Firebase
+		}
+	}
+	1.4.Overview of the app: {
+		- List ninjas
+		- Add/remove ninjas
+		- Filter ninjas
+	}
+	1.5.Atom: {
+		- atom.io
+		- packages: {
+			+ platformio-ide-terminal
+			+  language-typescript-grammar-only
+		}
+	}
+	1.6. github.com/iamshaunjp/angular-2-playlist
+}
