ec6176caba0ddf759a13b90e262506b97cf0034e
anhvurz90 <anhvurz90@gmail.com>
2017-03-10 14:56:03 +0700

    Add tutorial#2: Installing the Angular CLI

--- tutorial/tutorial.java
+++ tutorial/tutorial.java
@@ -36,3 +36,24 @@
 	}
 	1.6. github.com/iamshaunjp/angular-2-playlist
 }
+2.Installing the Angular CLI : {
+	2.1.The Angular CLI: {
+		- Angular commands:
+			+ Create new projects/ generate core file setup
+			+ Spin up a local server
+			+ Create new components
+		- Requires Node Package Manager
+	}
+	2.2.Download NPM: nodejs.org
+	2.3.Install Angular CLI: {
+		- npm install -g angular-cli
+	}
+	2.4.Create project: {
+		ng new ninja-directory
+	}
+	2.5.Spin up a local server: {
+		- cd ninja-directory
+		- ng serve
+		-> localhost:4200
+	}
+}
