b9f45d76eb9748d9196b6215f96f859e5fc0f84d
anhvurz90 <anhvurz90@gmail.com>
2017-03-15 11:37:01 +0700

    Add tutorial#14.code: Send data from AppComponent to its child HomeComponent

--- ninja-directory/src/app/app.component.ts
+++ ninja-directory/src/app/app.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app works!, woop woop.';
   name = 'ninja';
+
+  ninja = {
+    name : 'Vu',
+    belt: 'Red'
+  }
 }
--- ninja-directory/src/app/home/home.component.ts
+++ ninja-directory/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -15,6 +15,8 @@ export class HomeComponent implements OnInit {
     name: 'Yoshi',
     belt: 'red'
   };
+
+  @Input() ninjaParam;
 
   alertMe(msg) {
     alert(msg);
