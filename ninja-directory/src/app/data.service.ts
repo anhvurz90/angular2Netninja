bfe88395013f322693aa8e833b48fb1b4258f1a8
anhvurz90 <anhvurz90@gmail.com>
2017-04-07 11:05:24 +0700

    Add tutorial#25.code: Fetch data from Firebase

--- ninja-directory/src/app/data.service.ts
+++ ninja-directory/src/app/data.service.ts
@@ -7,6 +7,7 @@ export class DataService {
   constructor(private http: Http) { }
 
   fetchData() {
-    return this.http.get("/assets/ninjas.json");
+    //return this.http.get("/assets/ninjas.json");
+    return this.http.get("https://nanochat-9e1d4.firebaseio.com/.json");
   }
 }
